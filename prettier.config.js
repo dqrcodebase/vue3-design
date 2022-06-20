4040651b3e99128a1e80c42658fa96e3d8676b9f
D Q R Mac <852601818@qq.com>
2022-06-20 18:01:36 +0800

    no message

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,7 @@ module.exports = {
   vueIndentScriptAndStyle: false, // vue文件中的script和style标签的缩进
   singleQuote: true, // 字符串使用单引号
   trailingComma: 'all', // 对象末尾有没有逗号
-  proseWrap: 'always', // 代码块换行
-  htmlWhitespaceSensitivity: 'strict',
-  endOfLine: 'auto',
+  proseWrap: 'never', // 代码块换行
+  htmlWhitespaceSensitivity: 'strict', // html空白区域的处理
+  endOfLine: 'auto', // 换行符类型
 };

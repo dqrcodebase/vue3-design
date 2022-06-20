0585bff3ff1c6af0e1600ae3968513a02550fc16
D Q R Mac <852601818@qq.com>
2022-06-20 20:09:50 +0800

    6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how 
+      For a guide and recipes on how
       to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -70,12 +70,12 @@
 </template>
 
 <script>
-export default {
+ export default {
   name: 'HelloWorld',
   props: {
     msg: String,
   },
-};
+ };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

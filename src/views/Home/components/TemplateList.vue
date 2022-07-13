0aa51e521914cf6103481bc5810553795fc5c209
D Q R Mac <852601818@qq.com>
2022-07-13 10:53:58 +0800

    列表组件头部使用插槽

--- src/views/Home/components/TemplateList.vue
+++ src/views/Home/components/TemplateList.vue
@@ -10,35 +10,42 @@
     </div>
 
     <div class="design-gallery-list">
-      <el-scrollbar v-if="isGroup">
-        <div class="list-wrap group">
-          <div v-for="item in groupRecommendTemplateList" :key="item.kId">
-            <div class="space-between list-head">
-              <span>{{ item.name }}</span>
-              <span class="more-button" @click="moreHandle(item)"
-                >更多<el-icon :size="14"><ArrowRight /></el-icon
-              ></span>
+      <div v-if="isGroup" class="group-wrap">
+        <aside-list-skeleton v-if="getListloading" />
+        <el-scrollbar>
+          <div class="list-wrap group">
+            <div v-for="item in groupRecommendTemplateList" :key="item.kId">
+              <list-component
+                class="list-component"
+                :isShowFooter="false"
+                :list="groupItemList(item, index)">
+                t
+                <div class="space-between list-head">
+                  <span>{{ item.name }}</span>
+                  <span class="more-button" @click="moreHandle(item)"
+                    >更多<el-icon :size="14"><ArrowRight /></el-icon
+                  ></span>
+                </div>
+              </list-component>
             </div>
-            <list-component
-              class="list-component"
-              :isShowFooter="false"
-              :list="groupItemList(item, index)" />
           </div>
-        </div>
-      </el-scrollbar>
+        </el-scrollbar>
+      </div>
+
       <div class="list-wrap not-group" v-else>
-        <div class="list-head">
-          <span class="back-button" @click="backGroup"
-            ><el-icon :size="14"><ArrowLeft /></el-icon
-            >{{ noGroupData.name }}</span
-          >
-        </div>
         <list-component
           class="list-component"
           :list="noGroupData.list"
           :loading="getListloading"
           :noMore="noMore"
-          @load="getMoreData" />
+          @load="getMoreData">
+          <div class="list-head">
+            <span class="back-button" @click="backGroup"
+              ><el-icon :size="14"><ArrowLeft /></el-icon
+              >{{ noGroupData.name }}</span
+            >
+          </div></list-component
+        >
       </div>
     </div>
   </div>
@@ -55,17 +62,24 @@ import {
 } from 'vue';
 import { useAsideStore } from '@/store/aside';
 import ListComponent from '@/components/list.vue';
+import AsideListSkeleton from '@/components/AsideListSkeleton.vue';
 import FiltrateList from './FiltrateList.vue';
 
 export default {
-  components: { ListComponent, FiltrateList },
+  components: { ListComponent, FiltrateList, AsideListSkeleton },
+  beforeRouteEnter(to) {
+    console.log('to', to);
+    // 在渲染该组件的对应路由被验证前调用
+    // 不能获取组件实例 `this` ！
+    // 因为当守卫执行时，组件实例还没被创建！
+  },
   setup() {
     const store = useAsideStore();
     const activeName = ref('recommend');
     const isGroup = ref(true);
     const noMore = ref(false);
-    const getListloading = ref(false);
-    const groupRecommendTemplateList = reactive([]);
+    const getListloading = ref(true);
+    const groupRecommendTemplateList = ref([]);
     // const groupItemList = reactive([]);
     const noGroupData = reactive({
       name: '',
@@ -89,9 +103,8 @@ export default {
         pageSize: 50,
         templateType: 1,
       }).then((res) => {
-        console.log(res);
         getListloading.value = false;
-        groupRecommendTemplateList.push(...res.data);
+        groupRecommendTemplateList.value.push(...res.data);
       });
     }
     function getTemplateList() {
@@ -109,22 +122,20 @@ export default {
       noGroupData.name = item.name;
       noGroupData.list = [...item.items];
       noGroupData.kId = item.kId;
-      console.log(noGroupData, item);
     }
     function backGroup() {
       isGroup.value = true;
       getMoreDataParems.pageIndex = 1;
       noGroupData.list = [];
       noMore.value = false;
-      console.log(noGroupData);
     }
     function getMoreData() {
       getTemplateList();
     }
     function groupItemList(item) {
       const value = asideIsMini.value
-        ? item.items.slice(0, 4)
-        : item.items.slice(0, 12);
+        ? item.items.filter((it, index) => index < 4)
+        : item.items.filter((it, index) => index < 12);
       return value;
     }
     watch(asideIsMini, (newAsideIsMini, oldAsideIsMini) => {
@@ -144,6 +155,7 @@ export default {
       moreHandle,
       backGroup,
       getMoreData,
+      getListloading,
     };
   },
 };
@@ -153,6 +165,10 @@ export default {
 .design-gallery-list {
   margin-top: 12px;
   height: calc(100% - 106px - 28px);
+  overflow: hidden;
+  .group-wrap {
+    height: 100%;
+  }
 }
 .design-gallery-top {
 }
@@ -182,7 +198,7 @@ export default {
     height: calc(100% - 22px);
   }
   .list-component {
-    padding-top: 20px;
+    // padding-top: 20px;
   }
 }
 .template-components {
